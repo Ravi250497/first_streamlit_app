18794b4b6e66d436526e1afa4a5c7a4d7fd8c3ce
Ravi Raj <116078396+Ravi250497@users.noreply.github.com>
2022-10-26 15:18:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,4 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_response)
