fa158f3ce2121c0bd3d67d7060ec312ef83c7800
Ravi Raj <116078396+Ravi250497@users.noreply.github.com>
2022-10-27 10:45:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,12 +57,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("FRUIT LOAD LIST CONTAINS")
 streamlit.dataframe(my_data_rows)
 
-#Adding second box entry
-# changing to table format
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#streamlit.dataframe(fruityvice_normalized)
-#streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.header("Fruityvice Fruit Advice!")
 #import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+
