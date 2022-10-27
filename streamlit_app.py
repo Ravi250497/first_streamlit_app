39841d42bc214a582eb1a14530bb076b3a7db067
Ravi Raj <116078396+Ravi250497@users.noreply.github.com>
2022-10-27 10:55:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("FRUIT LOAD LIST CONTAINS")
 streamlit.dataframe(my_data_rows)
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
